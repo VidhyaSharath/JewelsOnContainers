948e22873ef568c47d29115a183d0f0f4e63c338
Vidhya S <vidhya.sambandan@yahoo.com>
2020-06-27 08:32:48 -0700

    Add project files.

---
+++ ProductCatalogAPI/Controllers/CatalogController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductCatalogAPI.Data;
+
+namespace ProductCatalogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private readonly CatalogContext _context;
+        public CatalogController(CatalogContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> Items(
+            [FromQuery]int pageIndex = 0,
+            [FromQuery]int pageSize = 6)
+        {
+            var items = await _context.CatalogItems
+                .OrderBy(c => c.Name)
+                .Skip(pageIndex * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            return Ok(items);
+
+        } 
+    }
+}
